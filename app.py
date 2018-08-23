2fea76239a23e6dacbe1892ec998f1145e011d1e
wesucceedme <wesucceedme@gmail.com>
2018-08-22 17:21:04 -0700

    Update export endpoints

--- app.py
+++ app.py
@@ -25,8 +25,22 @@ def webhook():
 
     return 'ok', 200
 
-@app.route('/exports/<path:endpoint>', methods=['POST'])
-def madden_export(endpoint):
+@app.route('/exports/<system>/<leagueId>/leagueTeams', methods=['POST'])
+def league_teams_export(system, leagueId):
+    data = request.get_json()
+    print(data)
+
+    return 'ok', 200
+
+@app.route('/exports/<system>/<leagueId>/standings', methods=['POST'])
+def standings_export(system, leagueId):
+    data = request.get_json()
+    print(data)
+
+    return 'ok', 200
+
+@app.route('/exports/<system>/<leagueId>/week/<weekType>/<weekNumber>/<dataType>', methods=['POST'])
+def week_export(system, leagueId, weekType, weekNumber, dataType):
     data = request.get_json()
     print(data)
 
