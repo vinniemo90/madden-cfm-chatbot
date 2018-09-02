4dbe81ce8f2ef9f0892ec5b4cbc06b68e2736d21
wesucceedme <wesucceedme@gmail.com>
2018-09-01 17:16:40 -0700

    Unpad private key line terminators

--- app.py
+++ app.py
@@ -20,20 +20,17 @@ firebase_creds = {
   "type": os.getenv('FIREBASE_CREDS_TYPE'),
   "project_id": os.getenv('FIREBASE_PROJECT_ID'),
   "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
-  "private_key": os.getenv('FIREBASE_PRIVATE_KEY'),
+  "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
   "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
   "client_id": os.getenv('FIREBASE_CLIENT_ID'),
   "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
   "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
   "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER'),
-  "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_CERT_URL')
+  "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_CERT_URL'),
 }
 
-with open(FIREBASE_CERT_FILE, 'w') as fp:
-    fp.write(json.dumps(firebase_creds))
-
 # Initialize firebase connection
-cred = credentials.Certificate(FIREBASE_CERT_FILE)
+cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred, {
     'databaseURL' : os.getenv('DATABASE_URL')
 })
