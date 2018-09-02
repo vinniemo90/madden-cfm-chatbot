1a4d25ced89654bc1126101118f0a4617b0baa30
wesucceedme <wesucceedme@gmail.com>
2018-09-02 11:52:55 -0700

    Add help and teams command

--- app.py
+++ app.py
@@ -28,6 +28,9 @@ firebase_creds = {
   "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_CERT_URL'),
 }
 
+# Team info to be returned
+team_info_dict = {'City':'', 'Mascot':'','Division':'','Ovr Rating':'','Owner':''}
+
 # Initialize firebase connection
 cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred, {
@@ -46,11 +49,43 @@ def webhook():
         msg = f"{data['name']}, you sent '{data['text']}'"
         send_message(msg)
 
-    elif '/rules' in data['text'].lower():
-        with open('cfm-rules.json') as rules:
-            cfm_rules = json.load(rules)
-            league_rules = cfm_rules['league rules']
-            send_message('\n'.join(league_rules))
+    elif data['name'] != 'John Madden' and '/help' in data['text'].lower():
+        help_prompt = get_help_prompt()
+        send_message('\n'.join(help_prompt))
+
+    elif data['name'] != 'John Madden' and '/rules' in data['text'].lower():
+        league_rules = get_league_rules()
+        send_message('\n'.join(league_rules))
+
+    elif data['name'] != 'John Madden' and '/teams' in data['text'].lower():
+        msg = data['text'].lower().split()
+        func_index = msg.index('/teams')
+        if(len(msg) > func_index + 1):
+            try:
+                team_map_snapshot = cfm.child('teamMap').get()
+                team_id = team_map_snapshot[msg[func_index + 1]]
+                team_info_snapshot = cfm.child('teams').child(team_id).get()
+
+                team_info_dict['City'] = team_info_snapshot['cityName']
+                team_info_dict['Mascot'] = team_info_snapshot['displayName']
+                team_info_dict['Division'] = team_info_snapshot['divName']
+                team_info_dict['Ovr Rating'] = team_info_snapshot['ovrRating']
+                if team_info_snapshot['userName']:
+                    team_info_dict['Owner'] = team_info_snapshot['userName']
+                else:
+                    team_info_dict['Owner'] = 'CPU'
+
+                send_message(team_info_dict)
+            
+            except Exception as e:
+                print(e)
+                send_message('Sorry, an error occurred processing your request.')
+
+        else:
+            send_message("Sorry, I couldn't find a team name associated with your request."
+                        " Use '/help' to get a list of commands.")
+
+
 
     return 'ok', 200
 
@@ -84,9 +119,8 @@ def standings_export(system, leagueId):
     del data
 
     standings_ref = cfm.child('standings')
-    
+
     for team in standings:
-        
         standings_ref.update({team['teamId']: team})
 
     return 'ok', 200
@@ -116,3 +150,13 @@ def send_message(msg):
 
     request = Request(url, urlencode(data).encode())
     json = urlopen(request).read().decode()
+
+def get_league_rules():
+    with open('cfm-rules.json') as rules:
+            cfm_rules = json.load(rules)
+            return cfm_rules['league rules']
+
+def get_help_prompt():
+    with open('cfm-rules.json') as rules:
+            cfm_rules = json.load(rules)
+            return cfm_rules['help prompt']
