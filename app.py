004d42247e67c57c4b76d2acdc218a499b87e895
wesucceedme <wesucceedme@gmail.com>
2018-09-02 13:16:07 -0700

    Add cap injuries and record enpoints

--- app.py
+++ app.py
@@ -31,6 +31,9 @@ firebase_creds = {
 # Team info to be returned
 team_info_dict = {'City':'', 'Mascot':'','Division':'','Ovr Rating':'','Owner':''}
 
+# Team cap info to be returned
+team_cap_dict = {'Team':'', 'Cap Room': '', 'Cap Spent': '', 'Cap Available': ''}
+
 # Initialize firebase connection
 cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred, {
@@ -49,14 +52,17 @@ def webhook():
         msg = f"{data['name']}, you sent '{data['text']}'"
         send_message(msg)
 
+    # Help prompt
     elif data['name'] != 'John Madden' and '/help' in data['text'].lower():
         help_prompt = get_help_prompt()
         send_message('\n'.join(help_prompt))
 
+    # Rules prompt
     elif data['name'] != 'John Madden' and '/rules' in data['text'].lower():
         league_rules = get_league_rules()
         send_message('\n'.join(league_rules))
 
+    # Team info
     elif data['name'] != 'John Madden' and '/teams' in data['text'].lower():
         msg = data['text'].lower().split()
         func_index = msg.index('/teams')
@@ -70,12 +76,82 @@ def webhook():
                 team_info_dict['Mascot'] = team_info_snapshot['displayName']
                 team_info_dict['Division'] = team_info_snapshot['divName']
                 team_info_dict['Ovr Rating'] = team_info_snapshot['ovrRating']
+
                 if team_info_snapshot['userName']:
                     team_info_dict['Owner'] = team_info_snapshot['userName']
                 else:
                     team_info_dict['Owner'] = 'CPU'
 
-                send_message(team_info_dict)
+                team_info = [ f'{key}: {val}' for key, val in team_info_dict.items() ]
+                send_message('\n'.join(team_info))
+            
+            except Exception as e:
+                print(e)
+                send_message('Sorry, an error occurred processing your request.')
+
+        else:
+            send_message("Sorry, I couldn't find a team name associated with your request."
+                        " Use '/help' to get a list of commands.")
+
+    # Team record
+    elif data['name'] != 'John Madden' and '/record' in data['text'].lower():
+        msg = data['text'].lower().split()
+        func_index = msg.index('/record')
+        if(len(msg) > func_index + 1):
+            try:
+                team_map_snapshot = cfm.child('teamMap').get()
+                team_id = team_map_snapshot[msg[func_index + 1].lower()]
+                team_standings_snapshot = cfm.child('standings').child(team_id).get()
+
+                team_record = f"{team_standings_snapshot['teamName']}: {team_standings_snapshot['totalWins']}-{team_standings_snapshot['totalLosses']}-{team_standings_snapshot['totalTies']} ({team_standings_snapshot['divWins']}-{team_standings_snapshot['divLosses']}-{team_standings_snapshot['divTies']})"
+                send_message(team_record)
+            
+            except Exception as e:
+                print(e)
+                send_message('Sorry, an error occurred processing your request.')
+
+        else:
+            send_message("Sorry, I couldn't find a team name associated with your request."
+                        " Use '/help' to get a list of commands.")
+
+    # Team cap info
+    elif data['name'] != 'John Madden' and '/cap' in data['text'].lower():
+        msg = data['text'].lower().split()
+        func_index = msg.index('/cap')
+        if(len(msg) > func_index + 1):
+            try:
+                team_map_snapshot = cfm.child('teamMap').get()
+                team_id = team_map_snapshot[msg[func_index + 1].lower()]
+                team_standings_snapshot = cfm.child('standings').child(team_id).get()
+
+                team_cap_dict['Team'] = team_standings_snapshot['teamName']
+                team_cap_dict['Cap Room'] = team_standings_snapshot['capRoom']
+                team_cap_dict['Cap Spent'] = team_standings_snapshot['capSpent']
+                team_cap_dict['Cap Available'] = team_standings_snapshot['capAvailable']
+
+                team_cap = [ f'{key}: {val}' for key, val in team_cap_dict.items() ]
+                send_message('\n'.join(team_cap))
+            
+            except Exception as e:
+                print(e)
+                send_message('Sorry, an error occurred processing your request.')
+
+        else:
+            send_message("Sorry, I couldn't find a team name associated with your request."
+                        " Use '/help' to get a list of commands.")
+    
+    # Number of injured players
+    elif data['name'] != 'John Madden' and '/injuries' in data['text'].lower():
+        msg = data['text'].lower().split()
+        func_index = msg.index('/injuries')
+        if(len(msg) > func_index + 1):
+            try:
+                team_map_snapshot = cfm.child('teamMap').get()
+                team_id = team_map_snapshot[msg[func_index + 1].lower()]
+                team_info_snapshot = cfm.child('teams').child(team_id).get()
+
+                msg = f"{data['name']}, you sent '{data['text']}'"
+                send_message(f"{team_info_snapshot['displayName']} have {team_info_snapshot['injuryCount']} players injured")
             
             except Exception as e:
                 print(e)
