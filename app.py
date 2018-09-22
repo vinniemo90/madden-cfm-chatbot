c439abe60c48e5d84be29ecf9258cbce1f4ca2dc
wesucceedme <wesucceedme@gmail.com>
2018-09-21 23:25:15 -0700

    Add advance logic

--- app.py
+++ app.py
@@ -42,6 +42,19 @@ team_stats_dict = {'Team':'','Team Rank':'', 'Prev. Team Rank':'', 'Pts For Rank
 # Team cap info to be returned
 team_cap_dict = {'Team':'', 'Cap Room': '', 'Cap Spent': '', 'Cap Available': ''}
 
+# Preseason (4 weeks)
+pre_dict = {'weekNumber': 1, 'weekType': 'pre'}
+
+# Regular season (17 weeks)
+reg_dict = {'weekNumber': 1, 'weekType': 'reg'}
+
+# Playoffs (5 weeks including pro bowl)
+playoffs_dict = {'weekNumber': 18, 'weekType': 'playoffs'}
+
+# Offseason (7 weeks - Resign players, free agency (4 weeks), Draft, Offseason recap)
+offseason_dict = {'weekNumber': 23, 'weekType': 'offseason'}
+offseason_stages = ['Resign Players', 'Free Agency', 'Free Agency', 'Free Agency', 'Free Agency', 'Draft', 'Offseason Recap']
+
 # Initialize firebase connection
 cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred, {
@@ -57,34 +70,156 @@ def webhook():
 
     # Update users on GroupMe name change
     if data['sender_id'].lower() == 'system' and ('changed name to' in data['text'].lower() or 'added' in data['text'].lower()):
+        print('Groupme user changed name or new user added to group')
         request_params = {'token': groupme_token}
         group_members = requests.get(f'https://api.groupme.com/v3/groups/{groupme_group_id}', params = request_params).json()['response']['members']
-        
+        print('Updating groupme users info')
         cfm.update({'groupMeUsers': group_members})
+
+    # Weekly Advance
+    elif data['name'] != 'John Madden' and '/advance' in data['text'].lower():
+        print('Advance keyword found')
+        msg = data.lower().split()
+        func_index = msg.index('/advance')
+        schedule = []
+        if(len(msg) > func_index + 1) and (msg[func_index + 1].lower() == 'pre'):
+            try:
+                print('Advancing to the beginning of preseason')
+                cfm.update({'league': pre_dict})
+                schedule.append('Preseason Week 1 Schedule')
+                schedule = schedule + get_user_games('pre', '1')
+                send_message('\n'.join(schedule))
+            
+            except Exception as e:
+                print(e)
+                send_message('Sorry, an error occurred processing your request.')
+
+        elif(len(msg) > func_index + 1) and (msg[func_index + 1].lower() == 'reg'):
+            try:
+                print('Advancing to the beginning of regular season')
+                cfm.update({'league': reg_dict})
+                schedule.append('Regular Season Week 1 Schedule')
+                schedule = schedule + get_user_games('reg', '1')
+                send_message('\n'.join(schedule))
+            
+            except Exception as e:
+                print(e)
+                send_message('Sorry, an error occurred processing your request.')
+
+        elif(len(msg) > func_index + 1) and (msg[func_index + 1].lower() == 'playoffs'):
+            try:
+                print('Advancing to the beginning of playoffs')
+                cfm.update({'league': playoffs_dict})
+                schedule.append('Wildcard Schedule')
+                schedule = schedule + get_user_games('reg', '18')
+                send_message('\n'.join(schedule))
+            
+            except Exception as e:
+                print(e)
+                send_message('Sorry, an error occurred processing your request.')
+
+        elif(len(msg) > func_index + 1) and (msg[func_index + 1].lower() == 'offseason'):
+            try:
+                print('Advancing to the beginning of offseason')
+                cfm.update({'league': offseason_dict})
+                send_message('Offseason Stage 1: Resign Players')
+            
+            except Exception as e:
+                print(e)
+                send_message('Sorry, an error occurred processing your request.')
+
+        else:
+            try:
+                league_snapshot = cfm.child('league').get()
+                week_type = league_snapshot['weekType']
+                week_number = league_snapshot['weekNumber']
+
+                if week_type == 'pre':
+                    week_number += 1
+                    if week_number < 5:
+                        week_dict = {'weekNumber': week_number, 'weekType': 'pre'}
+                        cfm.update({'league': week_dict})
+                        schedule.append(f'Preason Week {week_number} Schedule')
+                        schedule = schedule + get_user_games('pre', str(week_number))
+                        send_message('\n'.join(schedule))
+                    else:
+                        cfm.update({'league': reg_dict})
+                        schedule.append(f'Regular Season Week 1 Schedule')
+                        schedule = schedule + get_user_games('reg', '1')
+                        send_message('\n'.join(schedule))
+
+                elif week_type == 'reg':
+                    week_number += 1
+                    if week_number < 18:
+                        week_dict = {'weekNumber': week_number, 'weekType': 'reg'}
+                        cfm.update({'league': week_dict})
+                        schedule.append(f'Regular Season Week {week_number} Schedule')
+                        schedule = schedule + get_user_games('reg', str(week_number))
+                        send_message('\n'.join(schedule))
+                    else:
+                        cfm.update({'league': playoffs_dict})
+                        schedule.append('Wildcard Schedule')
+                        schedule = schedule + get_user_games('reg', '18')
+                        send_message('\n'.join(schedule))
+
+                elif week_type == 'playoffs':
+                    week_number += 1
+                    if week_number == 21:
+                        send_message('Pro Bowl Week')
+                    elif week_number == 18:
+                        week_dict = {'weekNumber': week_number, 'weekType': 'playoffs'}
+                        cfm.update({'league': week_dict})
+                        schedule.append('Wildcard Schedule')
+                        schedule = schedule + get_user_games('reg', str(week_number))
+                        send_message('\n'.join(schedule))
+                    elif week_number == 19:
+                        week_dict = {'weekNumber': week_number, 'weekType': 'playoffs'}
+                        cfm.update({'league': week_dict})
+                        schedule.append('Divisional Schedule')
+                        schedule = schedule + get_user_games('reg', str(week_number))
+                        send_message('\n'.join(schedule))
+                    elif week_number == 20:
+                        week_dict = {'weekNumber': week_number, 'weekType': 'playoffs'}
+                        cfm.update({'league': week_dict})
+                        schedule.append('Conference Championship Schedule')
+                        schedule = schedule + get_user_games('reg', str(week_number))
+                        send_message('\n'.join(schedule))
+                    elif week_number == 22:
+                        week_dict = {'weekNumber': week_number, 'weekType': 'playoffs'}
+                        cfm.update({'league': week_dict})
+                        schedule.append('Super Bowl Schedule')
+                        schedule = schedule + get_user_games('reg', str(week_number))
+                        send_message('\n'.join(schedule))
+                    else:
+                        cfm.update({'league': offseason_dict})
+                        send_message('Offseason Stage 1: Resign Players')
+
+                else:
+                    week_number += 1
+                    if week_number < 30:
+                        week_dict = {'weekNumber': week_number, 'weekType': 'offseason'}
+                        cfm.update({'league': week_dict})
+                        offseason_stage = week_number - 22
+                        send_message(f'Offseason Stage {offseason_stage}: {offseason_stages[week_number-23]}')
+                    else:
+                        cfm.update({'league': pre_dict})
+                        schedule.append('Preseason Week 1 Schedule')
+                        schedule = schedule + get_user_games('pre', '1')
+                        send_message('\n'.join(schedule))
+            
+            except Exception as e:
+                print(e)
+                send_message('Sorry, an error occurred processing your request.')
 
     # User game schedule for the week
     elif data['name'] != 'John Madden' and ('/schedule week' in data['text'].lower() or '/schedule wk' in data['text'].lower()):
+        print('Schedule keyword found')
         msg = data['text'].lower().split()
         func_index = msg.index('/schedule')
         if(len(msg) > func_index + 2):
             try:
-                team_snapshot = cfm.child('teams').get()
-                user_team_ids = [ team['teamId'] for team_id, team in team_snapshot.items() if team['userName'] ]
-                schedule_snapshot = cfm.child(f'weeks/reg/{msg[func_index + 2]}/schedules').get()
-                
-                user_games = []
-                schedule = []
-                for game_info in schedule_snapshot:
-                    if game_info['awayTeamId'] in user_team_ids and game_info['homeTeamId'] in user_team_ids:
-                        user_games.append((game_info['homeTeamId'], game_info['awayTeamId']))
-
-                if user_games:
-                    for home_team_id, away_team_id in user_games:
-                        schedule.append(f"{team_snapshot[str(home_team_id)]['nickName']} vs. {team_snapshot[str(away_team_id)]['nickName']}")
-
-                else:
-                    schedule.append(f"No user vs. user games were found for week {msg[func_index + 2]}")
-                
+                print(f'Retrieving user game schedule for wk {msg[func_index + 2]}')
+                schedule = get_user_games('reg', msg[func_index + 2])
                 send_message('\n'.join(schedule))
             
             except Exception as e:
@@ -97,10 +232,12 @@ def webhook():
 
     # Standings
     elif data['name'] != 'John Madden' and '/standings' in data['text'].lower():
+        print('Standings keyword found')
         msg = data['text'].lower().split()
         func_index = msg.index('/standings')
         if(len(msg) > func_index + 1) and (msg[func_index + 1].lower() != 'nfl'):
             try:
+                print(f'Retrieving standings info for {msg[func_index + 1]} conf')
                 conf_map_snapshot = cfm.child('conferenceMap').get()
                 conf_id = conf_map_snapshot[msg[func_index + 1].lower()]
                 standings_info_snapshot = cfm.child('standings').get()
@@ -118,6 +255,7 @@ def webhook():
 
         else:
             try:
+                print('Retrieving standings info for entire nfl')
                 standings_info_snapshot = cfm.child('standings').get()
 
                 nfl_teams = [ (team['rank'], team['teamName']) for team_id, team in standings_info_snapshot.items() if int(team['rank']) < 19 ]
@@ -133,20 +271,24 @@ def webhook():
 
     # Help prompt
     elif data['name'] != 'John Madden' and '/help' in data['text'].lower():
+        print('Help keyword found')
         help_prompt = get_help_prompt()
         send_message('\n'.join(help_prompt))
 
     # Rules prompt
     elif data['name'] != 'John Madden' and '/rules' in data['text'].lower():
+        print('Rules keyword found')
         league_rules = get_league_rules()
         send_message('\n'.join(league_rules))
 
     # Team info
     elif data['name'] != 'John Madden' and '/teams' in data['text'].lower():
+        print('Teams keyword found')
         msg = data['text'].lower().split()
         func_index = msg.index('/teams')
         if(len(msg) > func_index + 1):
             try:
+                print(f'Retrieving team info for {msg[func_index + 1]}')
                 team_map_snapshot = cfm.child('teamMap').get()
                 team_id = team_map_snapshot[msg[func_index + 1].lower()]
                 team_info_snapshot = cfm.child('teams').child(team_id).get()
@@ -174,10 +316,12 @@ def webhook():
 
     # Team season stats
     elif data['name'] != 'John Madden' and '/stats' in data['text'].lower():
+        print('Stats keyword found')
         msg = data['text'].lower().split()
         func_index = msg.index('/stats')
         if(len(msg) > func_index + 1):
             try:
+                print(f'Retrieving team season stats for {msg[func_index + 1]}')
                 team_map_snapshot = cfm.child('teamMap').get()
                 team_id = team_map_snapshot[msg[func_index + 1].lower()]
                 team_info_snapshot = cfm.child('standings').child(team_id).get()
@@ -208,10 +352,12 @@ def webhook():
 
     # Team record
     elif data['name'] != 'John Madden' and '/record' in data['text'].lower():
+        print('Record keyword found')
         msg = data['text'].lower().split()
         func_index = msg.index('/record')
         if(len(msg) > func_index + 1):
             try:
+                print(f'Retrieving season record for {msg[func_index + 1]}')
                 team_map_snapshot = cfm.child('teamMap').get()
                 team_id = team_map_snapshot[msg[func_index + 1].lower()]
                 team_standings_snapshot = cfm.child('standings').child(team_id).get()
@@ -229,10 +375,12 @@ def webhook():
 
     # Team cap info
     elif data['name'] != 'John Madden' and '/cap' in data['text'].lower():
+        print('Cap keyword found')
         msg = data['text'].lower().split()
         func_index = msg.index('/cap')
         if(len(msg) > func_index + 1):
             try:
+                print(f'Retrieving salary cap info for {msg[func_index + 1]}')
                 team_map_snapshot = cfm.child('teamMap').get()
                 team_id = team_map_snapshot[msg[func_index + 1].lower()]
                 team_standings_snapshot = cfm.child('standings').child(team_id).get()
@@ -255,10 +403,12 @@ def webhook():
     
     # Number of injured players
     elif data['name'] != 'John Madden' and '/injuries' in data['text'].lower():
+        print('Injuries keyword found')
         msg = data['text'].lower().split()
         func_index = msg.index('/injuries')
         if(len(msg) > func_index + 1):
             try:
+                print(f'Retrieving injuries info for {msg[func_index + 1]}')
                 team_map_snapshot = cfm.child('teamMap').get()
                 team_id = team_map_snapshot[msg[func_index + 1].lower()]
                 team_info_snapshot = cfm.child('teams').child(team_id).get()
@@ -363,3 +513,30 @@ def get_help_prompt():
     with open('cfm-rules.json') as rules:
             cfm_rules = json.load(rules)
             return cfm_rules['help prompt']
+
+def get_user_games(week_type, week_number):
+    ''' Get user vs. user game schedule and return as list '''
+    try:
+        team_snapshot = cfm.child('teams').get()
+        user_team_ids = [ team['teamId'] for team_id, team in team_snapshot.items() if team['userName'] ]
+        schedule_snapshot = cfm.child(f'weeks/{week_type}/{week_number}/schedules').get()
+        
+        user_games = []
+        schedule = []
+        for game_info in schedule_snapshot:
+            if game_info['awayTeamId'] in user_team_ids and game_info['homeTeamId'] in user_team_ids:
+                user_games.append((game_info['homeTeamId'], game_info['awayTeamId']))
+
+        if user_games:
+            for home_team_id, away_team_id in user_games:
+                schedule.append(f"{team_snapshot[str(home_team_id)]['nickName']} vs. {team_snapshot[str(away_team_id)]['nickName']}")
+
+        else:
+            schedule.append(f"No user vs. user games were found for {week_type} week {week_number}")
+
+    except Exception as e:
+        print(e)
+        schedule.append('Sorry, an error occurred retrieving user games.')
+
+    finally:
+        return schedule
