02d91173c2ecf6866b6343a764777109f347aba2
Vincent Sanders <vincent.sandes@tg4.solutions>
2019-07-15 08:32:10 -0700

    Add debug messages for groupme module

--- groupme.py
+++ groupme.py
@@ -14,18 +14,22 @@ def send_message(msg, db_root = None):
     url = 'https://api.groupme.com/v3/bots/post'
 
     if(mentions_exists):
+        attachment = add_mentions_attachment(msg, db_root)
+        print(f'Groupme Attachment ===> {attachment}')
         data = {
             'bot_id'    : os.getenv('GROUPME_BOT_ID'),
             'text'      : msg,
-            'attachments': [add_mentions_attachment(msg, db_root)]
+            'attachments': [attachment]
         }
 
     else:
+        print('No user mentions found')
         data = {
             'bot_id'    : os.getenv('GROUPME_BOT_ID'),
             'text'      : msg
         }
 
+    print('Sending groupme message')
     request = Request(url, urlencode(data).encode())
     json = urlopen(request).read().decode()
 
@@ -41,10 +45,12 @@ def get_groupme_user_ids(mentions, db_root):
     groupme_users_snapshot = [ user.get('user_id') for user in groupme_users_snapshot]
 
     for user in mentions:
+        print('Retrieving groupme ids for mentioned users')
         try:
             user_index = groupme_users_snapshot.index(user[1:])
             groupme_ids.append(groupme_users_snapshot[user_index])
         except ValueError:
+            print(f'{user} was mentioned but user does not exists')
             groupme_ids.append('')
 
     return groupme_ids
@@ -53,12 +59,15 @@ def add_mentions_attachment(msg, db_root):
     user_ids = []
     loci = []
 
+    print('Finding all user mentions within message')
     mentions = re.findall('\((.*?)\)',msg)
     indices = [ msg.index(mention) for mention in mentions ]
     lengths = [ len(mention) for mention in mentions ]
 
+    print('Get groupme user ids for each mentioned user')
     groupme_user_ids = get_groupme_user_ids(mentions, db_root)
 
+    print('Generate groupme attachment dictionary')
     for index, user_id in enumerate(groupme_user_ids):
         if user_id:
             user_ids.append(user_id)
