edc4e2a2bb595f00cf76ee96ae034a3bd94ed9b3
Vincent Sanders <vincent.sandes@tg4.solutions>
2019-10-26 09:24:53 -0700

    add function to get team nicknames

--- utils.py
+++ utils.py
@@ -21,3 +21,14 @@ def get_help_prompt():
     with open('cfm-rules.json') as rules:
         cfm_rules = json.load(rules)
         return '\n'.join(cfm_rules['help prompt'])
+
+def get_team_nicknames(teamAbbreviation):
+    '''Get nicknames associated with NFL team
+    
+    Returns:
+        List -- Nicknames associated with team
+    '''
+
+    with open('teamNicknames.json') as teamNicknames:
+        nicknames = json.load(teamNicknames)
+        return nicknames['teamAbbreviation']
